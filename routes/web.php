ccc0c4a62322c369d42a347a9cac6a4e9c008e22
chakib_2002 <abdelmalek.feddal@worldlearningalgeria.org>
2023-05-14 12:11:15 +0100

    set up the db schema + create fake products + render view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\ProductController::class, 'index']);
+Route::post('/checkout', [App\Http\Controllers\CheckoutController::class, 'checkout'])->name('checkout');
