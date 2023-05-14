93f864a2cbaa11d5b8e738700e631675597930d6
chakib_2002 <abdelmalek.feddal@worldlearningalgeria.org>
2023-05-14 12:42:47 +0100

    implementing stripe

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Order;
 
 class ProductController extends Controller
 {
@@ -13,4 +14,43 @@ class ProductController extends Controller
        $products = Product::all();
        return view('product.index', compact('products'));
     }
+
+    public function checkout()
+
+    {
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $products = Product::all();
+        $lineItems = [];
+        $totalPrice = 0;
+        foreach ($products as $product) {
+            $totalPrice += $product->price;
+            $lineItems[] = [
+                'price_data' => [
+                    'currency' => 'usd',
+                    'product_data' => [
+                        'name' => $product->name,
+                        'images' => [$product->image]
+                    ],
+                    'unit_amount' => $product->price * 100,
+                ],
+                'quantity' => 1,
+            ];
+        }
+        $session = \Stripe\Checkout\Session::create([
+            'line_items' => $lineItems,
+            'mode' => 'payment',
+            'success_url' => route('checkout.success', [], true) . "?session_id={CHECKOUT_SESSION_ID}",
+            'cancel_url' => route('checkout.cancel', [], true),
+        ]);
+
+        $order = new Order();
+        $order->status = 'unpaid';
+        $order->total_price = $totalPrice;
+        $order->session_id = $session->id;
+        $order->save();
+
+        return redirect($session->url);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\ProductController::class, 'index']);
-Route::post('/checkout', [App\Http\Controllers\CheckoutController::class, 'checkout'])->name('checkout');
+Route::post('/checkout', [App\Http\Controllers\ProductController::class, 'checkout'])->name('checkout');
+Route::get('/success', [App\Http\Controllers\ProductController::class, 'success'])->name('checkout.success');
+Route::get('/cancel', [App\Http\Controllers\ProductController::class, 'cancel'])->name('checkout.cancel');
